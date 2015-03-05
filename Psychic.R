3ba38376b78e63ed22e73f94a1f4fa17908f8501
Hrishi.Olickel <hrishioa@gmail.com>
2015-03-05 12:50:02 +0800

    Added more observations to Psychic.R

--- Psychic.R
+++ Psychic.R
@@ -4,7 +4,7 @@ library(ggplot2)
 #Load observed data
 psychic <- c(4,4,7,4,8,1,6,6,5,13,4,7,10)
 
-#Generate Random Data
+#Generate Random Data 
 rand = rbinom(13,25,0.2)
 
 #Compile data into data frame
@@ -12,3 +12,9 @@ psy <- data.frame(psy = psychic, rand = rand)
 
 #Plot
 ggplot(data=psy)+geom_histogram(aes(x=psychic,fill='Observed'),binwidth=2)+geom_histogram(alpha=0.5,aes(x=rand,fill='Random'),binwidth=2)
+
+#new data frame
+psy2 <- data.frame(psy=rep(psychic,100),rand=rbinom(1300,25,0.2))
+
+#new plot
+ggplot(data=psy2)+geom_histogram(aes(x=psychic,fill='Observed'),binwidth=2)+geom_histogram(alpha=0.5,aes(x=rand,fill='Random'),binwidth=2)
