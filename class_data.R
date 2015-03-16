e5e7d7c21f6f4c7a5530482f794032db2021991f
Hrishi.Olickel <hrishioa@gmail.com>
2015-03-16 12:11:09 +0800

    completed sanitation

--- class_data.R
+++ class_data.R
@@ -1,5 +1,7 @@
 class_data <- read.csv("H:/Downloads/The QR Correlation Questionnaire (Responses) - Form Responses 1.csv")
-View(`The.QR.Correlation.Questionnaire.(Responses)...Form.Responses.1`)
-
 class_data <- class_data[-7,]
+View(class_data)
 
+names(class_data) <- c('timestamp','sleep','happy','2d4d','haircolor','age','height','weight','coffee','questions','leftfoot','pulse')
+
+View(class_data)
