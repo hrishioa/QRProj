4d082c348bc0c3c19e5e1d513c534abe94dab35f
Hrishi.Olickel <hrishioa@gmail.com>
2015-03-16 12:04:01 +0800

    Started project tabulating class data

---
+++ class_data.R
@@ -0,0 +1,5 @@
+class_data <- read.csv("H:/Downloads/The QR Correlation Questionnaire (Responses) - Form Responses 1.csv")
+View(`The.QR.Correlation.Questionnaire.(Responses)...Form.Responses.1`)
+
+class_data <- class_data[-7,]
+
