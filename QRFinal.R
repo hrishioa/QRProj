b82b597e1e7b7193276744b8881a4666140810e5
Hrishi Olickel <hrishioa@199-151.priv23.nus.edu.sg>
2015-04-21 20:40:18 +0800

    QR final Project files

---
+++ QRFinal.R
@@ -0,0 +1,85 @@
+HExpen <- read.csv("~/Dropbox/Yale-NUS College/Y1Sem2/QR/HealthExpenPerCapita.csv")
+HExpen <- HExpen[,c(1,17)]
+LifeExpect <- read.csv("~/Dropbox/Yale-NUS College/Y1Sem2/QR/LifeExpectancy.csv")
+LifeExpect <- LifeExpect[,c(1,206)]
+PopDens <- read.csv("~/Dropbox/Yale-NUS College/Y1Sem2/QR/PopulationDensity.csv")
+PopDens <- PopDens[,c(1,62)]
+
+
+HExpen <- HExpen[complete.cases(HExpen), ]
+
+LifeExpect <- LifeExpect[complete.cases(LifeExpect), ]
+
+PopDens <- PopDens[complete.cases(PopDens), ]
+
+
+#Find common elements
+LE <- LifeExpect
+LE <- subset(LE,LE$Year %in% PopDens$Year)
+LE <- subset(LE,LE$Year %in% HExpen$X1995)
+PD <- PopDens
+PD <- subset(PD,PD$Year %in% HExpen$X1995)
+PD <- subset(PD,PD$Year %in% LifeExpect$Year)
+HE <- HExpen
+HE <- subset(HE,HE$X1995 %in% LifeExpect$Year)
+HE <- subset(HE,HE$X1995 %in% PopDens$Year)
+
+#Sort
+HE <- HE[order(HE[,1]),]
+LE <- LE[order(LE[,1]),]
+PD <- PD[order(PD[,1]),]
+
+#Combine the Datasets for the current Year
+
+
+
+#Supplementary Info
+Supp_GDP <- read.csv("~/Dropbox/Yale-NUS College/Y1Sem2/QR/Supp_GDP.csv")
+Supp_HDI <- read.csv("~/Dropbox/Yale-NUS College/Y1Sem2/QR/Supp_HDI.csv")
+Supp_BMI <- read.csv("~/Dropbox/Yale-NUS College/Y1Sem2/QR/Supp_BMI.csv")
+
+
+#Get only the current year
+Supp_GDP <- Supp_GDP[,c(1,49)]
+Supp_HDI <- Supp_HDI[,c(1,10)]
+Supp_BMI <- Supp_BMI[,c(1,30)]
+
+#Sanitize
+Supp_GDP <- Supp_GDP[complete.cases(Supp_GDP), ]
+Supp_BMI <- Supp_BMI[complete.cases(Supp_BMI), ]
+Supp_HDI <- Supp_HDI[complete.cases(Supp_HDI), ]
+
+#Intersect
+S_GDP <- Supp_GDP
+S_HDI <- Supp_HDI
+S_BMI <- Supp_BMI
+
+S_GDP <- subset(S_GDP,S_GDP$Year %in% S_BMI$Year)
+S_GDP <- subset(S_GDP,S_GDP$Year %in% S_HDI$Year)
+
+S_HDI <- subset(S_HDI,S_HDI$Year %in% S_BMI$Year)
+S_HDI <- subset(S_HDI,S_HDI$Year %in% S_GDP$Year)
+
+S_BMI <- subset(S_BMI,S_BMI$Year %in% S_HDI$Year)
+S_BMI <- subset(S_BMI,S_BMI$Year %in% S_GDP$Year)
+
+#select 30 countries closest to singapore after sorting
+S_GDP <- S_GDP[order(S_GDP[,2]),]
+S_HDI <- S_HDI[order(S_HDI[,2]),]
+S_BMI <- S_BMI[order(S_BMI[,2]),]
+
+ssize <- 35
+
+S_BMI <- S_BMI[((60-ssize):(60+ssize)),] #singapore - 60, unintersected - 67
+S_HDI <- S_HDI[((148-ssize):(148+ssize)),] #singapore - 148, unintersected - 177
+S_GDP <- S_GDP[((160-ssize):(160+ssize)),] #Singapore - 160, unintersected - 162
+
+#Intersect again
+S_GDP <- subset(S_GDP,S_GDP$Year %in% S_BMI$Year)
+S_GDP <- subset(S_GDP,S_GDP$Year %in% S_HDI$Year)
+
+S_HDI <- subset(S_HDI,S_HDI$Year %in% S_BMI$Year)
+S_HDI <- subset(S_HDI,S_HDI$Year %in% S_GDP$Year)
+
+S_BMI <- subset(S_BMI,S_BMI$Year %in% S_HDI$Year)
+S_BMI <- subset(S_BMI,S_BMI$Year %in% S_GDP$Year)
--- QRWritingAssignment.R
+++ QRWritingAssignment.R
@@ -9,14 +9,11 @@ QRSCV <- QRSCV[complete.cases(QRSCV), ]
 #Start graphing
 library(ggplot2)
 
-#Graph histograms to see relationship
-
 #Graph the density plot to see relationship
 ggplot(data=QRSCV)+geom_density(data=QRSCV[QRSCV$Gender==1,],aes(x=Loneliness,fill='Male'),alpha=1)+geom_density(data=QRSCV[QRSCV$Gender==2,],aes(x=Loneliness,fill='Female'),alpha=0.5)+geom_density(data=QRSCV[QRSCV$Gender==3,],aes(x=Loneliness,fill='Neither'),alpha=0.5)+scale_fill_manual(name="Gender",labels=c('Male','Female','N/A'),values=c('Red','Blue','Green'))
 
 #Separate the two into different columns and collect statistics
 Male = QRSCV[QRSCV$Gender==1,]$Loneliness
 Female = QRSCV[QRSCV$Gender==2,]$Loneliness
-
 
 t.test(Female, Male, mu=0, alt='greater', conf=0.95, paired=F)
